935aedf4dcb5e74c0187eacdfea540b551873ec0
jojochilly <joan_qin@126.com>
2019-02-22 17:45:48 +0800

    fix: use ts file as source code

--- test/libs/tinyPng.spec.ts
+++ test/libs/tinyPng.spec.ts
@@ -4,33 +4,32 @@
 import * as fs from 'fs';
 import * as path from 'path';
 import tinyPng from '../../libs/tinypng-io';
-import {expect} from '../chai-configuration';
+import { expect } from '../chai-configuration';
 
 describe('index', () => {
     describe('upload', () => {
         it('should upload successfully, pass in buffer', () => {
             const targetPath = path.resolve(__dirname, '../img/sample-03.png');
             let imgContent = fs.readFileSync(targetPath);
-            return tinyPng.upload({content: imgContent})
+            return tinyPng.upload({ content: imgContent });
         });
-
-        it('should upload successfully, but response invalid data type', (done) => {
-            tinyPng.upload({filename: '../img/invalid-img.gif'}).catch(({message}) => {
-                expect(message).to.be.equal(`{"error":"Unsupported media type","message":"File type is not supported"}`);
+        it('should upload successfully, but response invalid data type', done => {
+            tinyPng.upload({ filename: 'test/img/invalid-img.gif' }).catch(({ message }) => {
+                expect(message).to.be.equal(
+                    `{"error":"Unsupported media type","message":"File type is not supported"}`,
+                );
                 done();
             });
         });
     });
-
     describe('download', () => {
         it('should download successfully', () => {
-            let downloadUrl = `https://tinypng.com/web/output/fb3ktg0vxtq3wr2ak7b03wywr66wn81g`;
-            return tinyPng.download(downloadUrl)
+            let downloadUrl = `https://tinypng.com/web/output/75ra4uyqn2x4dwrdxxux3zf9aj9mdnet`;
+            return tinyPng.download(downloadUrl);
         });
-
-        it('should download error, unknown download url', (done) => {
+        it('should download error, unknown download url', done => {
             let downloadUrl = `https://tinypng.com/web/output/111111`;
-            tinyPng.download(downloadUrl).catch(({message}) => {
+            tinyPng.download(downloadUrl).catch(({ message }) => {
                 expect(message).to.be.equal('Request failed with status code 404');
                 done();
             });
--- test/libs/utils.spec.ts
+++ test/libs/utils.spec.ts
@@ -1,5 +1,5 @@
-import {string2Uint8array} from '../../libs/utils';
-import {expect} from '../chai-configuration';
+import { string2Uint8array } from '../../libs/utils';
+import { expect } from '../chai-configuration';
 import tinyPng from '../../libs';
 /**
  * Created by allen on 2016/11/3.
@@ -9,7 +9,9 @@ describe('utils', () => {
     describe('string2Uint8array', () => {
         it('parameter(s) <hello> should derive return <null>', () => {
             const input = `hello`;
-            expect(string2Uint8array(input)).to.be.eqls({"0": 104, "1": 101, "2": 108, "3": 108, "4": 111});
+            expect(JSON.stringify(string2Uint8array(input))).to.equals(
+                '{"0":104,"1":101,"2":108,"3":108,"4":111}',
+            ); // [104, 101, 108, 108, 111]
         });
         it('parameter(s) <null> should derive return <null>', () => {
             const input = null;
@@ -21,22 +23,26 @@ describe('utils', () => {
         });
         it('parameter(s) <0> should derive return <null>', () => {
             const input = 0;
-            expect(() => string2Uint8array(input as any)).to.throw(`utils: unknown parameter type. expect parameter to be string`);
+            expect(() => string2Uint8array(input as any)).to.throw(
+                `utils: unknown parameter type. expect parameter type to be string`,
+            );
         });
         it('parameter(s) <""> should derive return <"">', () => {
-            const input = "";
+            const input = '';
             expect(string2Uint8array(input)).to.be.null;
         });
         it('parameter(s) <[]> should derive to throw', () => {
             const input = [];
-            expect(() => string2Uint8array(input as any)).to.be.throw(`utils: unknown parameter type. expect parameter to be string`);
+            expect(() => string2Uint8array(input as any)).to.be.throw(
+                `utils: unknown parameter type. expect parameter type to be string`,
+            );
         });
         it('parameter(s) <false> should derive to throw', () => {
             const input = false;
-            expect(() => string2Uint8array(input as any)).to.be.throw(`utils: unknown parameter type. expect parameter to be string`);
+            expect(() => string2Uint8array(input as any)).to.be.throw(
+                `utils: unknown parameter type. expect parameter type to be string`,
+            );
         });
     });
-
-    describe('uint8array2String', () => {
-    });
+    describe('uint8array2String', () => {});
 });
